b878553038606408b82eb81a6261dc01389e1c65
g13916kh <g13916kh@m20205.tsuda.ac.jp>
2016-06-24 18:54:17 +0900

    4プロ

---
+++ war/knock.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+
+	function ring() {
+		document.getElementById("KnockSound").play();
+	}
+
+	function OnButtonClick() {
+		//if(サーバーが立ち上がっているとき相手に音を伝える、ノック音)
+		a=1;
+		if(a==1){
+            alert('ノックしました');
+		}else{
+			alert('川村はいません');
+		}
+      }
+
+
